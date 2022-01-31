451bcffaf62e6bab0886c4b62945df1b5b1e2a1e
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-01-30 22:46:46 -0600

    Finish tests for EMployee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,24 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee obect', () => {
-	const employee = new Employee("Bob Dole", '501', 'bdole@Worker.com');
-	expect(employee.name).toEqual(expect.any(String));
-	expect(employee.id).toEqual(expect.any(String));
-	expect(employee.email).toEqual(expect.any(String));
+	const employee = new Employee();
+	expect(typeof(employee)).toBe('object');
 });
 
+// gets name
+test('gets employee name', () => {
+	const employee = new Employee("Bob Dole", '501', 'bdole@Worker.com');
+	expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// gets id
+test('gets employee id', () => {
+	const employee = new Employee("Bob Dole", '501', 'bdole@Worker.com');
+	expect(employee.getId()).toEqual(expect.any(String));
+});
+
+// gets email
+test('gets employee email', () => {
+	const employee = new Employee("Bob Dole", '501', 'bdole@Worker.com');
+	expect(employee.getEmail()).toEqual(expect.any(String));
+});
